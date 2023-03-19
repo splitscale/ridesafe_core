9c7c7f33431ff0c9ba79f092c784021155139703
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-19 23:52:40 +0800

    re-init

---
+++ bin/ridesafe_core.dart
@@ -0,0 +1,5 @@
+import 'package:ridesafe_core/ridesafe_core.dart' as ridesafe_core;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ridesafe_core.calculate()}!');
+}
---
+++ test/ridesafe_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:ridesafe_core/ridesafe_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
