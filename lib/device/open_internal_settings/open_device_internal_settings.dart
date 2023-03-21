c88c4c630396770945fe0114211ab0001c2a266f
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-21 19:47:13 +0800

    refactor open internal settings

--- lib/device/open_internal_settings/open_device_internal_settings.dart
+++ lib/device/open_internal_settings/open_device_internal_settings.dart
@@ -1,9 +1,9 @@
 import '../../services/device_service.dart';
 
-class DeviceOpenInternalSettings {
+class OpenDeviceInternalSettingsInteractor {
   final DeviceService _service;
 
-  DeviceOpenInternalSettings(this._service);
+  OpenDeviceInternalSettingsInteractor(this._service);
 
   Future<void> openInternalSettings() {
     return _service.openInternalSettings();
