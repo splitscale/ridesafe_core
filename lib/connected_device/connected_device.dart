bd00110633eb6bd5cf423e8b3bc97196b65a1576
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-22 19:16:06 +0800

    fix connected device bug

--- lib/connected_device/connected_device.dart
+++ lib/connected_device/connected_device.dart
@@ -3,9 +3,9 @@ import 'package:ridesafe_core/device/device.dart';
 class ConnectedDevice<T> extends Device {
   final T _conn;
 
-  ConnectedDevice(super.name, super.address, this._conn);
+  ConnectedDevice(super.name, super.address, super.isPaired, this._conn);
   ConnectedDevice.fromDevice(Device device, this._conn)
-      : super(device.name, device.address);
+      : super(device.name, device.address, device.isPaired);
 
   T get connection => _conn;
 }
