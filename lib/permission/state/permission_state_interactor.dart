b45451e7df941c50454ef2aaab14ea53c2ad5b83
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-22 11:37:31 +0800

    fixed perm status

--- lib/permission/state/permission_state_interactor.dart
+++ lib/permission/state/permission_state_interactor.dart
@@ -1,10 +1,10 @@
 import '../../services/permission_service.dart';
 import '../../services/state/service_state.dart';
 
-class PermissionRequestInteractor {
+class PermissionStatusInteractor {
   final PermissionService _service;
 
-  PermissionRequestInteractor(this._service);
+  PermissionStatusInteractor(this._service);
 
   Future<ServiceStatus> get status {
     return _service.status;
